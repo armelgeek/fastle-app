8f64e14954e431f52cda327de89235d0661c2dbd
Armel Geek <armelgeek5@gmail.com>
2021-06-06 12:52:58 +0300

    configuration de easy-peasy comme gestion de store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { StoreProvider } from "easy-peasy";
 import './index.css'
 import App from './App'
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreProvider store={store}>
     <App />
-  </React.StrictMode>,
+  </StoreProvider>,
   document.getElementById('root')
 )
